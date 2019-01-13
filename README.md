1f0c8f5dfd35c2c589475214bcb8801c28538069
maruju11 <maruju@gmail.com>
2019-01-13 15:49:47 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-web-server
+app-weather-maruju
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append file maruju')
+    }
+  });
+  next();
+});
+
+// app.use((req,res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to página da família Maruju',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) =>{
+  res.render('about.hbs', {
+    pageTitle: 'About page injection',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send('não encontramos a url solicitada');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port} - maruju`);
+});
